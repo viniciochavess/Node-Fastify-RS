5586acf12175b7eb221c17530476b7793ae3220a
DESKTOP-6P5NGT9\INFO <viniciochavess@gmail.com>
2024-06-08 08:57:25 -0300

    handler erro global

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import fastify from 'fastify';
 import { appRouterUser } from './router/UserRouter';
+import { ZodError } from 'zod';
 const app  = fastify();
 
 app.register(appRouterUser)
+
+app.setErrorHandler((error, request, reply) => {
+    if (error instanceof ZodError){
+        reply.code(400).send({message: "Validation Error", issues: error.format() })
+    }
+})
+
 
 export default app;
